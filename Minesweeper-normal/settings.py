1d38027f121c20c1c9b301ceea73af2f92adccd2
zaki <zakip1r1o13@gmail.com>
2025-01-21 13:29:19 +0100

    fixed a small problemo

--- Minesweeper-normal/settings.py
+++ Minesweeper-normal/settings.py
@@ -30,10 +30,10 @@ TITLE= "Minesweeper python"
 
 tile_numbers = []
 for i in range(1, 9):
-    tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", f"Tile{i}.png")),(TILESIZE, TILESIZE)))
-    tile_empty = pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", "TileEmpty.png")),(TILESIZE, TILESIZE))
-    tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", "TileExploded.png")),(TILESIZE, TILESIZE))
-    tile_flag = pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", "TileFlag.png")),(TILESIZE, TILESIZE))
-    tile_mine = pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", "TileMine.png")),(TILESIZE, TILESIZE))
-    tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", "TileNotMine.png")),(TILESIZE, TILESIZE))
-    tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("Minesweeper","assets", "TileUnknown.png")),(TILESIZE, TILESIZE))
+    tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("assets", f"Tile{i}.png")),(TILESIZE, TILESIZE)))
+    tile_empty = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileEmpty.png")),(TILESIZE, TILESIZE))
+    tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileExploded.png")),(TILESIZE, TILESIZE))
+    tile_flag = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileFlag.png")),(TILESIZE, TILESIZE))
+    tile_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileMine.png")),(TILESIZE, TILESIZE))
+    tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileNotMine.png")),(TILESIZE, TILESIZE))
+    tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileUnknown.png")),(TILESIZE, TILESIZE))
